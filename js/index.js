b42b6c0b9ebf584a40408a6cf43aba41fae70a0a
mytionBr <matheuss.jesus2017@gmail.com>
2021-06-06 00:03:58 -0300

    adding clear input function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
 let input_search = document.querySelector("#search_bar")
 let input_text_icons;
+let clear_icon =document.querySelector("#clear_icon")
+
+let actions_text_input_icons = {
+    addEffect: ()=>{
+        for( icon of input_text_icons){
+            console.log(icon)
+           clear_icon.style.cursor = "pointer"
+           icon.style.color="#70757a"
+        }
+    },
+    removeEffect: ()=>{
+        clear_icon.style.cursor = "default"
+        for( icon of input_text_icons){
+            icon.style.color="white"
+        }
+    }
+}
 
 window.addEventListener('load',()=>{
+    clear_icon =document.querySelector("#clear_icon")
     input_text_icons = update_icon_reference()
 })
 
@@ -16,20 +34,23 @@ input_search.addEventListener('focusout',()=>{
 
 input_search.addEventListener('input',()=>{
     if(input_search.value.length > 0){
-        for( icon of input_text_icons){
-           icon.style.color="#70757a"
-        }
+        actions_text_input_icons.addEffect()
     }else {
         for( icon of input_text_icons){
-            icon.style.color="white"
+            actions_text_input_icons.removeEffect()
         }
     }
 })
 
 input_search.addEventListener('inputout',()=>{
     for( icon of input_text_icons){
-        icon.style.color="white"
+        actions_text_input_icons.removeEffect()
     }
+})
+
+clear_icon.addEventListener('click',()=>{
+    input_search.value = ''
+    actions_text_input_icons.removeEffect()
 })
 
 function update_icon_reference(){
